8fe06d6ca24188b98686bec788233682ec9fa351
luchenqun <luchenqun@qq.com>
2017-03-29 15:46:32 +0800

    小的优化

--- js/lmbtfy.js
+++ js/lmbtfy.js
@@ -49,36 +49,47 @@ $(document).ready(function() {
         var $instructions = $('#instructions');
         var $arrow = $('#arrow');
         setTimeout(function() {
+
             $instructions.text('1、把鼠标放到输入框上');
-            $arrow.show().animate({
+            var styles = {
                 left: $kw.offset().left + 10 + 'px',
                 top: ($kw.offset().top + $kw.height() / 2) + 'px'
-            }, 2000, function() {
+            }
+
+            $arrow.show().animate(styles, 2000, function() {
                 $instructions.text('2、输入你的问题');
                 $arrow.hide();
                 var $kw = $('#kw');
                 $kw.focus();
                 var i = 0;
+                var delay = (kw.length >= 40) ? 100 : 200;
+
                 var interval = setInterval(function() {
-                    $kw.val(kw.substr(0, i));
-                    i++;
+                    $kw.val(kw.substr(0, i++));
                     if (i > kw.length) {
                         clearInterval(interval);
                         $instructions.text('3、按下“百度一下”按钮');
                         $arrow.show();
                         var $search = $('#search');
-                        $arrow.animate({
+                        styles = {
                             left: $search.offset().left + $search.width() / 2 + 'px',
                             top: $search.offset().top + $search.height() / 2 + 'px'
-                        }, 1000, function() {
-                            $instructions.html('<strong>这对你而言就是这么困难么？</strong>');
+                        };
+                        $arrow.animate(styles, 1000, function() {
+                            $instructions.html('这对你而言就是这么困难么？');
+
                             setTimeout(function() {
-                                window.location = 'http://www.baidu.com/s?tn=lmbtfy.cn&ch=3&ie=utf-8&wd=' + encodeURIComponent(kw);
+                                $instructions.html('<strong>见证奇迹的时刻到了！</strong>');
+                            }, 2000);
+
+                            setTimeout(function() {
+                                window.location = 'http://www.baidu.com/s?tn=mybookmark.cn&ch=3&ie=utf-8&wd=' + encodeURIComponent(kw);
                             }, 2000);
                         })
                     }
-                }, 200);
+                }, delay);
             });
+
         }, 1000);
     }
 });
